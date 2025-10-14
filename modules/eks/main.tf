f22e9717208aea1425330a81a4c66c8edaad2e53
Saurabh Khan <saurabhkhan@Saurabhs-MacBook-Air.local>
2025-10-14 21:00:31 +0530

    Fix EKS authentication with proper access entry and policy

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -59,6 +59,26 @@ resource "aws_eks_access_entry" "node" {
   type          = "EC2_LINUX"
 }
 
+data "aws_caller_identity" "current" {}
+
+resource "aws_eks_access_entry" "admin" {
+  cluster_name  = aws_eks_cluster.main.name
+  principal_arn = data.aws_caller_identity.current.arn
+  type         = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "admin" {
+  cluster_name  = aws_eks_cluster.main.name
+  principal_arn = data.aws_caller_identity.current.arn
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+
+  depends_on = [aws_eks_access_entry.admin]
+}
+
 resource "aws_eks_node_group" "main" {
   cluster_name    = aws_eks_cluster.main.name
   node_group_name = var.node_group_name
@@ -77,6 +97,8 @@ resource "aws_eks_node_group" "main" {
 
   depends_on = [
     aws_iam_role_policy_attachment.node,
-    aws_eks_access_entry.node
+    aws_eks_access_entry.node,
+    aws_eks_access_entry.admin,
+    aws_eks_access_policy_association.admin
   ]
 }
